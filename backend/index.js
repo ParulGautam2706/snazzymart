ff343bddb1863916117cb227f770bb607aebabde
ParulGautam27 <parulgautam9813@gmail.com>
2026-06-25 10:39:28 +0530

    fix: correct CORS origin to snazzymart netlify URL

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(
     origin: [
       'http://localhost:5173',
       'http://localhost:5174',
-      'https://shopease-010.netlify.app'
+      'https://snazzymart-im1.netlify.app'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
