48739aa93f59d0c5ce96302957209be697ef6931
ParulGautam27 <parulgautam9813@gmail.com>
2026-06-27 11:14:59 +0530

    Initial commit: SnazzyMart e-commerce website

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(
     origin: [
       'http://localhost:5173',
       'http://localhost:5174',
-      'https://snazzymart-im1.netlify.app'
+      'https://snazzymart-1.onrender.com'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
